3f3e83f3a3f9e4082c25f5cd31cd7def00dac3d8
Dale Jung <dale@dalejung.com>
2014-03-26 14:20:29 -0400

    WIP: general gist abstractions

--- nbx/nbmanager/gist.py
+++ nbx/nbmanager/gist.py
@@ -1,9 +1,11 @@
 from IPython.nbformat import current
 import github
 
+_missing = object()
+
 def model_to_files(model):
     """
-
+    Converts a IPython notebook model to a github.Gist `files` dict.
     Parameters
     __________
     model : dict
@@ -27,3 +29,101 @@ def model_to_files(model):
         f = github.InputFileContent(fn_content)
         files[fn] = f
     return files
+
+class GistService(object):
+    """
+    Keeps track of github accounts and gists.
+    """
+    def __init__(self):
+        self.hub = github.Github()
+        self.accounts = {}
+        self.default = None
+        self.gist_cache = {}
+
+    def login(self, login, password):
+        # if account already
+        if login in self.accounts:
+            return
+        hub = github.Github(login, password, user_agent="nbx")
+        self.accounts[login] = hub
+
+    def get_gist(self, gist_id, refresh=False):
+        if gist_id not in self.gist_cache or refresh:
+            gist = self._get_gist(gist_id)
+            self.gist_cache[gist_id] = gist
+        return self.gist_cache[gist_id]
+
+    def _get_gist(self, gist_id):
+        """
+        Returns Gist object. If we own the Gist, we return the authenticated
+        Gist. Otherwise return a read-only Gist.
+        """
+        gist = self.hub.get_gist(gist_id)
+        # if we're
+        owner = self.get_owner(gist)
+        if owner is not None:
+            gist = owner.get_gist(gist_id)
+        gist = Gister(gist, self)
+        return gist
+
+    def is_owned(self, gist):
+        """
+        Checks if the gist is ownd by an account we manage.
+        """
+        owner = self.get_owner(gist)
+        return owner is not None
+
+    def get_owner(self, gist):
+        login = gist.user.login
+        return self.accounts.get(login, None)
+
+class Gister(object):
+    def __init__(self, gist, service):
+        self.gist = gist
+        self.service = service
+
+    @property
+    def user(self):
+        return self.gist.user
+
+    def edit(self, description=None, files=None, force=False):
+        """
+        Wrapper around github.Gist.edit. If nothing has changed
+        we will not save unless `force` is true.
+        """
+        if description is None:
+            description = self.gist.description
+        dirty = False
+        for fn, fobj in self.gist.files.iteritems():
+            # if file is missing, means we leave it alone
+            if fn not in files:
+                continue
+            new_content = files[fn]
+            if new_content != fobj.content:
+                dirty = True
+                break
+        if description != self.gist.description:
+            dirty = True
+        #
+        if dirty or force:
+            self.gist.edit(description, files)
+
+    def save(self, description=None, files=_missing, force=False):
+        """
+        Similar to edit except it assumes that `files` represents all
+        files. So if a file is missing from `files` then it will be deleted.
+        """
+        if files is _missing:
+            raise Exception("files must be explicitly passed in")
+
+        if files is None:
+            files = {}
+
+        for fn, fobj in self.gist.files.iteritems():
+            if fn not in files:
+                files[fn] = None # mark for deletion
+
+        self.edit(description, files=files, force=force)
+
+    def pull(self):
+        self.gist = self.service.get_gist(self.gist.id)
--- nbx/nbmanager/tests/test_gist.py
+++ nbx/nbmanager/tests/test_gist.py
@@ -4,9 +4,9 @@ import nose.tools as nt
 from IPython.nbformat import current
 from IPython.html.services.notebooks.filenbmanager import FileNotebookManager
 from IPython.utils.tempdir import TemporaryDirectory
-import github
 
-from ..gist import model_to_files
+from ..gist import model_to_files, GistService, Gister
+from nbx.nbmanager.tests.common import *
 
 class TestGist(unittest.TestCase):
 
@@ -19,22 +19,107 @@ class TestGist(unittest.TestCase):
     def setUp(self):
         pass
 
+    def test_model_to_files(self):
+        with TemporaryDirectory() as td:
+            fm = FileNotebookManager(notebook_dir=td)
+            model = fm.create_notebook()
+            model = fm.get_notebook(model['name'], model['path'])
+            files = model_to_files(model)
+            name = model['name']
+            # files should only contain one file
+            nt.assert_items_equal(files, [name])
+
+            # add a file
+            model['__files'] = {'file1.txt': 'file1txt content'}
+            files = model_to_files(model)
+            nt.assert_items_equal(files, [name, 'file1.txt'])
+            nt.assert_equal(files['file1.txt']._InputFileContent__content,
+                            'file1txt content')
+
+class TestGistService(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs)
+
+    def runTest(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_get_gist(self):
+        gs = GistService()
+        gist_id = '9751912'
+        gist = gs.get_gist(gist_id)
+        nt.assert_equal(gist.user.login, 'dalejung')
+
+    def test_is_owned(self):
+        """ check whether gist is owned by local account """
+        gs = GistService()
+        gist_id = '9751912'
+        gist = gs.get_gist(gist_id)
+        nt.assert_false(gs.is_owned(gist))
+        # note, until we do something that requires auth
+        # it won't error
+        gs.login('dalejung', 'fakepw')
+        nt.assert_true(gs.is_owned(gist))
+
+class TestGister(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs)
+
+    def runTest(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_save(self):
+        gist = makeFakeGist()
+        gister = Gister(gist, None)
+
+        # delete all files
+        gister.save(files=None)
+        nt.assert_equal(gist.edit.call_count, 1)
+        description, files = gist.edit.call_args[0]
+        for fn in files:
+            nt.assert_is_none(files[fn])
+
+        gist = makeFakeGist()
+        gister = Gister(gist, None)
+        # add a new file, edit a.ipynb
+        gister.save(files={'new.txt':'new.txt content', 'a.ipynb':'new content'})
+        description, files = gist.edit.call_args[0]
+        nt.assert_items_equal(files, ['new.txt', 'a.ipynb', 'b.ipynb', 'test.txt'])
+
+        for fn in files:
+            if fn == 'new.txt':
+                nt.assert_equal(files[fn], 'new.txt content')
+            elif fn == 'a.ipynb':
+                nt.assert_equal(files[fn], 'new content')
+            else:
+                nt.assert_is_none(files[fn])
+
 with TemporaryDirectory() as td:
     fm = FileNotebookManager(notebook_dir=td)
     model = fm.create_notebook()
     model = fm.get_notebook(model['name'], model['path'])
-    files = model_to_files(model)
-    name = model['name']
-    # files should only contain one file
-    nt.assert_items_equal(files, [name])
-
-    # add a file
     model['__files'] = {'file1.txt': 'file1txt content'}
-    files = model_to_files(model)
-    nt.assert_items_equal(files, [name, 'file1.txt'])
-    nt.assert_equal(files['file1.txt']._InputFileContent__content,
-                    'file1txt content')
 
+    files = model_to_files(model)
+
+gs = GistService()
+gist_id = '9751912'
+gist = gs.get_gist(gist_id)
+gs.login(login, password)
+hub = gs.accounts['dalejung']
+
+gist = makeFakeGist()
+gister = Gister(gist, None)
+gister.save(files={'new.txt':'new.txt content'})
+args = gist.edit.call_args[0]
+description, files = args
 
 if __name__ == '__main__':
     import nose
