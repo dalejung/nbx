e9202e3ea15285d219719128a4c592c6a691bcc5
Dale Jung <dale@dalejung.com>
2014-03-27 14:20:45 -0400

    updated gist middleware to work with normal FileNotebookManager

--- nbx/nbmanager/gistmiddleware.py
+++ nbx/nbmanager/gistmiddleware.py
@@ -6,8 +6,15 @@ from IPython.utils.traitlets import (
 
 from .gist import GistService, model_to_files
 from .bundle.bundlenbmanager import BundleNotebookManager
+from IPython.html.services.notebooks.filenbmanager import FileNotebookManager
 
 class GistMiddleware(LoggingConfigurable):
+    """
+    This middleware will save to github if the notebook metadata contains a
+    gist_id.
+
+    This requires that the Gist is owned by an account we control.
+    """
     github_accounts = List(Tuple, config=True,
                             help="List of Tuple(github_account, github_password)")
 
@@ -20,7 +27,7 @@ class GistMiddleware(LoggingConfigurable):
 
     def post_save_notebook(self, nbm, local_path, model, name, path):
         # for now only support bundlenbmanager
-        if not isinstance(nbm, BundleNotebookManager):
+        if not isinstance(nbm, (BundleNotebookManager, FileNotebookManager)):
             return
 
         gist_id = model['content']['metadata'].get('gist_id', None)
@@ -30,10 +37,15 @@ class GistMiddleware(LoggingConfigurable):
         if not self.service.is_owned(gist):
             return
 
-        model = nbm.get_notebook(name, local_path, content=True, file_content=True)
+        try:
+            # this is only applicable to bundles
+            model = nbm.get_notebook(name, local_path, content=True, file_content=True)
+        except:
+            model = nbm.get_notebook(name, local_path, content=True)
+
         files = model_to_files(model)
         gist.save(description=name, files=files)
-        msg = "Saved Bundle {path} {name} to gist {gist_id}".format(name=name,
+        msg = "Saved notebook {path} {name} to gist {gist_id}".format(name=name,
                                                             path=local_path,
                                                             gist_id=gist_id)
         self.log.info(msg)
