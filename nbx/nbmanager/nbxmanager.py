c67edc6081cb53d317ee2bf2f34e63ca4eef4ef4
Dale Jung <dale@dalejung.com>
2014-09-10 19:57:52 -0400

    Start up of NBXManager dispatching. So far save/get_model

--- nbx/nbmanager/nbxmanager.py
+++ nbx/nbmanager/nbxmanager.py
@@ -17,12 +17,13 @@ class NBXContentsManager(ContentsManager):
 
         fullpath = self.fullpath(name, path)
         if self.is_dir(fullpath):
-            model = self.get_model_dir(name, path, content)
+            model_type = 'dir'
         elif name.endswith('.ipynb'):
-            model = self.get_model_notebook(name, path, content)
+            model_type = 'notebook'
         else:
-            model = self.get_model_file(name, path, content)
-        return model
+            model_type = 'file'
+        return self._dispatch_method('get_model', model_type, name=name,
+                              path=path, content=content)
 
     def _base_model(self, name, path=''):
         """Build the common base of a contents model"""
@@ -34,4 +35,35 @@ class NBXContentsManager(ContentsManager):
         model['last_modified'] = datetime.datetime.now()
         model['content'] = None
         model['format'] = None
+        return model
+
+    def save(self, model, name='', path=''):
+        if 'type' not in model:
+            raise Exception(u"Model has no file type")
+        model_type = model['type']
+        self._dispatch_method('save', model_type, model, name=name, path=path)
+
+    def _dispatch_method(self, hook, model_type, *args, **kwargs):
+        print('dispatch_method', hook, model_type)
+        # call type specific hook
+        method_name = "{hook}_{type}".format(hook=hook, type=model_type)
+        method = getattr(self, method_name, None)
+        if method:
+            return method(*args, **kwargs)
+
+        # try default
+        default_name = '{hook}_default'.format(hook=hook)
+        default_method = getattr(self, default_name)
+        return default_method(model, name, path)
+
+    def get_model_dir(self, name, path='', content=True):
+        """ retrofit to use old list_dirs. No notebooks """
+        model = self._base_model(name, path)
+        fullpath = url_path_join(name, path)
+
+        model['type'] = 'directory'
+        dirs = self.list_dirs(fullpath)
+        notebooks = self.list_notebooks(fullpath)
+        entries = dirs + notebooks
+        model['content'] = entries
         return model
