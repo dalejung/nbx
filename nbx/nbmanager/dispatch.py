824f3eb26f6a421bbaca92fe520cfdeff5966f69
Dale Jung <dale@dalejung.com>
2014-09-11 13:41:08 -0400

    Refactored the dispatcher logic to its own module

---
+++ nbx/nbmanager/dispatch.py
@@ -0,0 +1,70 @@
+"""
+Tools to help create dispatched methods for the Contents API.
+
+The Contents api has a single function like save, get_model that
+handles multiple types (file, notebook, dir).
+
+The main logic is in normal functions that the Mixin wraps. This is so the
+logic can be used easily in the Middleware object.
+
+Not 100% on this.
+"""
+def _model_type_from_path(path):
+    if self.is_dir(path):
+        model_type = 'dir'
+    elif self.is_notebook(path):
+        model_type = 'notebook'
+    else:
+        model_type = 'file'
+    return model_type
+
+def dispatch_method(self, hook, model_type, *args, **kwargs):
+    print('dispatch_method', hook, model_type)
+    # call type specific hook
+    method_name = "{hook}_{type}".format(hook=hook, type=model_type)
+    method = getattr(self, method_name, None)
+    if method:
+        return method(*args, **kwargs)
+
+    # try default
+    default_name = '{hook}_default'.format(hook=hook)
+    default_method = getattr(self, default_name)
+    return default_method(model, name, path)
+
+def dispatch_get_model(self, name, path='', content=True, dispatcher=dispatch_method):
+    """
+    Relies on:
+        is_dir
+        is_notebook
+    """
+    path = path.strip('/')
+
+    fullpath = self.fullpath(name, path)
+    model_type = _model_type_from_path(fullpath)
+    return dispatcher(self, 'get_model', model_type, name=name,
+                            path=path, content=content)
+
+def dispatch_save(self, model, name='', path='', dispatcher=dispatch_method):
+    if 'type' not in model:
+        raise Exception(u"Model has no file type")
+    model_type = model['type']
+    return dispatcher(self, 'save', model_type, model, name=name, path=path)
+
+class DispatcherMixin(object):
+    """
+    This Mixin class handles the dispatching of Contents API calls to
+    model type specific methods.
+
+    i.e. get_model for a directory would call get_model_dir
+    """
+
+    def save(self, model, name='', path=''):
+        return dispatch_save(self, model, name, path,
+                             dispatcher=self.dispatch_method)
+
+    def get_model(self, name, path='', content=True):
+        return dispatch_get_model(self, name, path, content, 
+                                  dispatcher=self.dispatch_method)
+
+    def dispatch_method(self, hook, model_type, *args, **kwargs):
+        return dispatch_method(self, hook, model_type, *args, **kwargs)
--- nbx/nbmanager/nbxmanager.py
+++ nbx/nbmanager/nbxmanager.py
@@ -3,30 +3,38 @@ import datetime
 from IPython.html.services.contents.manager import ContentsManager
 from IPython.html.utils import url_path_join
 
-class NBXContentsManager(ContentsManager):
+from .dispatch import DispatcherMixin
+
+def _fullpath(name, path):
+    fullpath = url_path_join(path, name)
+    return fullpath
+
+class BackwardsCompatMixin(object):
+    # shims to bridge Content service and older notebook apis
+    def get_model_dir(self, name, path='', content=True):
+        """ retrofit to use old list_dirs. No notebooks """
+        model = self._base_model(name, path)
+        fullpath = _fullpath(name, path)
+
+        model['type'] = 'directory'
+        dirs = self.list_dirs(fullpath)
+        notebooks = self.list_notebooks(fullpath)
+        entries = dirs + notebooks
+        model['content'] = entries
+        return model
+
+    def get_model_notebook(self, name, path='', content=True):
+        return self.get_notebook(name, path, content=content)
+
+class NBXContentsManager(DispatcherMixin, BackwardsCompatMixin, ContentsManager):
+    def __init__(self, *args, **kwargs):
+        super(NBXContentsManager, self).__init__(*args, **kwargs)
 
     def is_dir(self, path):
         raise NotImplementedError('must be implemented in a subclass')
 
     def is_notebook(self, path):
         return path.endswith('.ipynb')
-
-    def fullpath(self, name, path):
-        fullpath = url_path_join(path, name)
-        return fullpath
-
-    def get_model(self, name, path='', content=True):
-        path = path.strip('/')
-
-        fullpath = self.fullpath(name, path)
-        if self.is_dir(fullpath):
-            model_type = 'dir'
-        elif self.is_notebook(fullpath):
-            model_type = 'notebook'
-        else:
-            model_type = 'file'
-        return self._dispatch_method('get_model', model_type, name=name,
-                              path=path, content=content)
 
     def _base_model(self, name, path=''):
         """Build the common base of a contents model"""
@@ -39,38 +47,3 @@ class NBXContentsManager(ContentsManager):
         model['content'] = None
         model['format'] = None
         return model
-
-    def save(self, model, name='', path=''):
-        if 'type' not in model:
-            raise Exception(u"Model has no file type")
-        model_type = model['type']
-        return self._dispatch_method('save', model_type, model, name=name, path=path)
-
-    def _dispatch_method(self, hook, model_type, *args, **kwargs):
-        print('dispatch_method', hook, model_type)
-        # call type specific hook
-        method_name = "{hook}_{type}".format(hook=hook, type=model_type)
-        method = getattr(self, method_name, None)
-        if method:
-            return method(*args, **kwargs)
-
-        # try default
-        default_name = '{hook}_default'.format(hook=hook)
-        default_method = getattr(self, default_name)
-        return default_method(model, name, path)
-
-    # shims to bridge Content service and older notebook apis
-    def get_model_dir(self, name, path='', content=True):
-        """ retrofit to use old list_dirs. No notebooks """
-        model = self._base_model(name, path)
-        fullpath = self.fullpath(name, path)
-
-        model['type'] = 'directory'
-        dirs = self.list_dirs(fullpath)
-        notebooks = self.list_notebooks(fullpath)
-        entries = dirs + notebooks
-        model['content'] = entries
-        return model
-
-    def get_model_notebook(self, name, path='', content=True):
-        return self.get_notebook(name, path, content=content)
