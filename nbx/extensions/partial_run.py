6ecabe1e20ef7d82651bf91ccfc30bcb77bcb968
Dale Jung <dale@dalejung.com>
2016-02-21 21:38:04 -0500

    Update to use ipython kernel extension

--- nbx/extensions/partial_run.py
+++ nbx/extensions/partial_run.py
@@ -1,4 +1,9 @@
 import six
-# only run for python 3, or else syntax errors
-if six.PY3:
-    from ._partial_run import *
+
+
+def load_ipython_extension(shell):
+    # only run for python 3, or else syntax errors
+    if not six.PY3:
+        return
+    from ._partial_run import safe_run_module
+    shell.safe_run_module = safe_run_module.__get__(shell)
